95a1be820be40ddd836f9613286eadea8d445096
jansorianowork <jansoriano.work@gmail.com>
2025-07-09 23:47:11 +0800

    foundry learning

---
+++ script/DeploySimpleStorage.s.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {Script} from "forge-std/Script.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
+
+contract DeploySimpleStorage is Script {
+    // function deploy() public returns (address) {
+    //     address simpleStorage = address(new SimpleStorage());
+    //     return simpleStorage;
+    // }
+    function run() external returns (SimpleStorage){
+        vm.startBroadcast();
+        SimpleStorage simpleStorage = new SimpleStorage();
+        vm.stopBroadcast();
+        return simpleStorage;
+        // console.log("SimpleStorage deployed at:", address(simpleStorage));
+    }
+}
